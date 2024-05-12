920edf31370d2972fa3d563acc8a8d074d686c7d
Robin <165838816+robinbro71@users.noreply.github.com>
2024-05-12 15:55:40 +0700

    Delete proyek_akhir_pemrograman_dasar_web directory

--- proyek_akhir_pemrograman_dasar_web/script.js
+++
@@ -1,8 +0,0 @@
-function showSideBar() {
-    const sideBar = document.querySelector('.sideBar') 
-    sideBar.style.width = '250px'      
-}
-function hideSideBar() {
-    const sideBar = document.querySelector('.sideBar') 
-    sideBar.style.width = '0px'    
-}
